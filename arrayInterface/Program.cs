00e7ee553115dc28edf750044cc19d742d8693ab
wane228 <56423171+wane228@users.noreply.github.com>
2020-11-05 16:09:37 +1000

    Add files via upload

---
+++ arrayInterface/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+
+namespace obertka
+{
+    interface IArray
+    {
+        void Add(int newValue);
+        int Find(int index);
+        void Remove(int index);
+        void Update(int index, int newValue);
+    }
+
+    class Obertka : IArray
+    {
+        public int[] Array1 = { 1, 2, 3, 4, 5, 6, 7 };
+        public void Add(int newValue)
+        { 
+            int lenght = Array1.Length;
+            Array.Resize<int>(ref Array1, lenght+1);
+            Array1[lenght] = newValue;
+        }
+
+        public int Find(int index)
+        {
+            if (index > Array1.Length - 1 || index < 0)
+            {
+                Console.WriteLine("Error");
+                return 0;
+            }
+            int lenght = Array1.Length;
+            int temp = 0;
+            for(int i = 0; i < lenght - 1; i++)
+            {
+                if(i + 1 == index)
+                {
+                    temp = Array1[i];
+                }
+            }
+            return temp;
+        }
+        public void Remove(int index)
+        {
+            if (index > Array1.Length - 1 || index < 0)
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+            int[] temp = Array1;
+            Array.Resize<int>(ref temp, Array1.Length - 1);
+            int j = 0;
+            for(int i =0; i < Array1.Length ;i++ )
+            {
+                if (i != index-1)
+                {
+                    temp[j] = Array1[i];
+                    j++;
+                }
+            }
+            Array1 = temp;
+        }
+        public void Update(int index, int newValue)
+        {
+            if(index > Array1.Length - 1 || index < 0  )
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+            Array1[index - 1] = newValue;
+        }
+    }
+
+    class Programm
+    {
+        static void Main()
+        {
+            Obertka a = new Obertka();
+
+            for (int i = 0; i < a.Array1.Length; i++)
+            {
+                Console.Write(a.Array1[i]);
+            }
+            Console.WriteLine();
+
+            a.Add(9);
+            a.Add(9);
+            a.Add(9);
+
+            for(int i =0; i < a.Array1.Length; i++)
+            {
+                Console.Write(a.Array1[i]);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ fail/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace verstkafaila
+{
+    class Readfromfile
+    {
+        //public string number;
+        //public string name;
+        //public string fname;
+        //public string phone;
+        //public string uid;
+        //public string nik;
+        //public string wo;
+        //public string[] linestruct = new string[7];
+        public void Read(string path)
+        {
+            if (!(File.Exists(path)))
+            {
+                Console.WriteLine("Ошибка:: Файла не существует");
+                return;
+            }
+
+            StreamReader sr = new StreamReader(path);
+            string line;
+            Console.Write("Введите данные поиска: ");
+            string data = Console.ReadLine();
+
+            if (data == "")
+            {
+                Console.WriteLine("Ошибка:: отсутствуют данные для поиска");
+                return;
+            }
+
+            double ellapledTicks = DateTime.Now.Ticks;
+
+            bool flag = false;
+
+            int count = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] linestruct = new string[7];
+                int separatorCount = 0;
+                for (int i = 0; i < line.Length - 1; i++)
+                {
+                    if (line[i] == 124)
+                    {
+                        separatorCount++;
+
+                        int j = i + 1;
+                        while (line[j] != 124 && j < line.Length - 1)
+                        {
+                            if (separatorCount == 1)
+                            {
+                                linestruct[0] += line[j];
+                                j++;
+                            }
+                            else if (separatorCount == 2)
+                            {
+                                linestruct[1] += line[j];
+                                j++;
+                            }
+                            else if (separatorCount == 3)
+                            {
+                                linestruct[2] += line[j];
+                                j++;
+                            }
+                            else if (separatorCount == 4)
+                            {
+                                linestruct[3] += line[j];
+                                j++;
+                            }
+                            else if (separatorCount == 5)
+                            {
+                                linestruct[4] += line[j];
+                                j++;
+                            }
+                            else if (separatorCount == 6)
+                            {
+                                linestruct[5] += line[j];
+                                j++;
+                            }
+                            else if (separatorCount == 7)
+                            {
+                                linestruct[6] += line[j];
+                                j++;
+                            }
+                            else if (separatorCount > 7)
+                            {
+                                j++;
+                            }
+                        }
+
+                        i = j - 1;
+                    }
+                }
+
+
+                for (int i = 0; i < linestruct.Length; i++)
+                {
+                    if (data == linestruct[i])
+                    {
+                        ellapledTicks = (DateTime.Now.Ticks - ellapledTicks) / 10000000;
+
+                        flag = true;
+
+                        if (separatorCount > 7)
+                        {
+                            Console.WriteLine("Error");
+                            return;
+                        }
+                        Console.WriteLine(line);
+                        Console.WriteLine($"Время поиска: {ellapledTicks}c");
+                        return;
+                    }
+                }
+
+                count++;
+
+                if(count == 200000)
+                {
+                    //Console.Clear();
+                    Console.WriteLine("Подождите, идёт поиск...");
+                    count = 0;
+                }
+            }
+
+            if (flag == false)
+            {
+                Console.WriteLine("Ничего не найдено");
+            }
+
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Укажите путь к файлу: ");
+            string path = Console.ReadLine();
+            Readfromfile a = new Readfromfile();
+            a.Read(path);
+        }
+    }
+}
